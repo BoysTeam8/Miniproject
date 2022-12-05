bc78330ff8ea125a5b6af6399422761ff21e8b26
BoysTeam8 <119870213+BoysTeam8@users.noreply.github.com>
2022-12-05 20:44:06 +0530

    Add files via upload

---
+++ MiniProject Code/Airline-Management-System/src/airline/management/system/Mainframe.java
@@ -0,0 +1,130 @@
+package airline.management.system;
+
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+
+public class Mainframe extends JFrame{
+
+    public static void main(String[] args) {
+        new Mainframe().setVisible(true);
+    }
+    
+    public Mainframe() {
+        super("AIRLINE RESERVATION MANAGEMENT SYSTEM");
+        initialize();
+    }
+
+    
+    private void initialize() {
+	
+        setForeground(Color.CYAN);
+        setLayout(null); 
+
+        JLabel NewLabel = new JLabel("");
+	NewLabel.setIcon(new ImageIcon(ClassLoader.getSystemResource("airline/management/system/icon/logo.jpg")));
+	NewLabel.setBounds(0, 0, 1920, 990); 
+	add(NewLabel); 
+        
+        JLabel AirlineManagementSystem = new JLabel("Welcome To Philippine Airline");
+	AirlineManagementSystem.setForeground(Color.BLUE);
+        AirlineManagementSystem.setFont(new Font("Tahoma", Font.PLAIN, 36));
+	AirlineManagementSystem.setBounds(700, 60, 1000, 55);
+	NewLabel.add(AirlineManagementSystem);
+		
+		
+        JMenuBar menuBar = new JMenuBar();
+	setJMenuBar(menuBar);
+		
+        JMenu AirlineSystem = new JMenu("AIRLINE SYSTEM");
+        AirlineSystem.setForeground(Color.BLUE);
+	menuBar.add(AirlineSystem);
+        
+        JMenu Ticket = new JMenu("TICKET");
+        Ticket.setForeground(Color.blue);
+	menuBar.add(Ticket);
+		
+        JMenuItem FlightDetails = new JMenuItem("FLIGHT_INFO");
+	AirlineSystem.add(FlightDetails);
+		
+	JMenuItem ReservationDetails = new JMenuItem("ADD_CUSTOMER_DETAILS");
+	AirlineSystem.add(ReservationDetails);
+		
+	JMenuItem PassengerDetails = new JMenuItem("JOURNEY_DETAILS");
+	AirlineSystem.add(PassengerDetails);
+		
+	JMenuItem SectorDetails_1 = new JMenuItem("PAYMENT_DETAILS");
+	AirlineSystem.add(SectorDetails_1);
+        
+        JMenuItem Ticket1 = new JMenuItem("BOOKING");
+        Ticket.add(Ticket1);
+		
+	JMenuItem Cancellation = new JMenuItem("CANCELLATION");
+	Ticket.add(Cancellation);
+		
+	
+		
+	JMenuItem Exit = new JMenuItem("EXIT");
+        AirlineSystem.add(Exit);
+        
+        Exit.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                System.exit(0);
+            }
+        });
+        
+        Ticket1.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent ae){
+                new ticket();
+            }
+	});
+		
+	FlightDetails.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent ae){
+                new Flight_Info();
+            }
+	});
+        
+	ReservationDetails.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent ae){
+                try {
+                    new Add_Customer();
+		} catch (Exception e) {
+                    e.printStackTrace();
+		} 
+            }
+	});
+		
+        PassengerDetails.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent ae){
+                try {
+                    new Journey_Details();
+                } catch (Exception e) {
+                    e.printStackTrace();
+		}
+            }
+	});
+		
+        SectorDetails_1.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent ae){
+                try {
+                    new Payment_Details();
+		} catch (Exception e) {
+                    e.printStackTrace();
+		}
+            }
+	});
+		
+        Cancellation.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent ae){
+                new Cancel();
+            }
+	});
+		
+        setSize(1950,1090);
+	setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
